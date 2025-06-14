805db5a24e31947b96d796c529287868c4aea1ba
Francis Sanguyo <87265596+NotAMadTheorist@users.noreply.github.com>
2025-06-14 09:45:09 +0800

    Delete Source_MTKinetics /rootDir.py

--- Source_MTKinetics /rootDir.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-ROOT_DIR = Path(__file__).resolve().parent.parent
-path_KineticsData = ROOT_DIR / "Instrument Output Files" / "Tyrosinase Kinetics Data, Group 3.csv"
-path_AbsorbanceTimeTable = ROOT_DIR / "Program Output Files" / "Tyrosinase Kinetics, Absorbance-Time Table.csv"
-path_MichaelisMentenParams = ROOT_DIR / "Program Output Files" / "Tyrosinase Kinetics, Michaelis-Menten Parameters.csv"
-path_Models_and_ReactionRates = ROOT_DIR / "Program Output Files" / ("Tyrosinase Kinetics, Summarized Model and "
-                                                                     "Reaction Rates.csv")
-path_TTest_for_LineweaverBurk = ROOT_DIR / "Program Output Files" / ("Tyrosinase Kinetics, T-Test for Slopes and "
-                                                                     "Intercepts in Lineweaver-Burk Plot.csv")
